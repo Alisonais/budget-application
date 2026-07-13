51ea76e2124900472ab5d4c08e3300abc444ccf1
AlisonAis <alisonais@ymail.com>
2026-07-12 22:41:38 -0300

    add query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-// import { Stepper } from "./components/Stepper";
-// import { BudgetData } from "./components/steps/BudgetData";
-// import { CarData } from "./components/steps/CarData";
-// import { PaymentData } from "./components/steps/PaymentData";
-// import { PersonalData } from "./components/steps/PersonalData";
-// import { Review } from "./components/steps/Review";
 
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "./components/Toster";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Router } from "./Router";
 
 export function App() {
+
+  const queryclient = new QueryClient({
+    defaultOptions:{
+      queries: {
+        staleTime: 5 * 60 * 1000, // 5minutos
+        refetchOnWindowFocus: false,
+        gcTime: 10 * 60 * 1000,  // 10 minutos
+      }
+    }
+  });
+
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Router />
-        <Toaster />
-      </BrowserRouter>
-    </AuthProvider>
-
-
+    <QueryClientProvider client={queryclient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Router />
+          <Toaster />
+        </BrowserRouter>
+      </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
